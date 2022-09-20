94e48d498ca655eedb8f6018dbab148078ac67ca
Inaê Rosa <inaedb.rosa@gmail.com>
2022-09-20 20:02:06 -0300

    fix: add validations

--- api/controller/hotelGuestController.js
+++ api/controller/hotelGuestController.js
@@ -1,5 +1,6 @@
 const Exception = require('./../errors/Exception')
 const Guest = require("../model/Guest");
+const ValidGuestId = require('../validations/ValidGuestId');
 
 class hotelGuestController {
     static async getAll(req,res) {
@@ -11,11 +12,10 @@ class hotelGuestController {
     static async getById(req, res){
         try{
             const { id } = req.params;
-            const guest = await Guest.findById({_id: id})
-            if(!guest) throw Exception.BadRequest('there isnt a hotel guest with this ID')
+            const guest = await ValidGuestId(id)
             res.status(200).json(guest)
         }catch(err){
-            res.status(err.status).json({message: err.message})
+            res.status(500).json({message: err.message})
         }
     }
 
@@ -25,15 +25,15 @@ class hotelGuestController {
             await guest.save()
             res.status(201).send(req.body)
         }catch(err){
-            res.status(err.status).json({message: err.message});
+            res.status(404).json({message: err.message});
         }
     }
 
     static async updateGuest(req,res) {
         try{
             const {id} = req.params;
-            const guest = await Guest.findByIdAndUpdate(id, req.body)
-            if(!guest) throw Exception.BadRequest('there isnt a hotel guest with this ID')
+            await ValidGuestId(id)
+            await Guest.findByIdAndUpdate(id, req.body)
             res.status(200).send(req.body)
         }catch(err){
             res.status(err.status).json({message: err.message})
@@ -43,8 +43,8 @@ class hotelGuestController {
     static async deleteGuest(req,res){
         try{
             const { id } = req.params;
-            const guest = await Guest.findByIdAndDelete(id);
-            if(!guest) throw Exception.BadRequest('there isnt a hotel guest with this ID')
+            await ValidGuestId(id)
+            await Guest.findByIdAndDelete(id);
             res.status(200).send({message: 'Guest removed succesfully'})
         }catch(err){
             res.status(err.status).json({message: err.message})
